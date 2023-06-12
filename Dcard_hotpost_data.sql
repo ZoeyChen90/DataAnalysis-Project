be493fc6a5dca9cfd13ad7d957c13825b4ebb11c
ZoeyChen90 <135522979+ZoeyChen90@users.noreply.github.com>
2023-06-12 17:00:07 +0800

    Update Dcard_hotpost_data.sql
    修改欄位名稱

--- Dcard_hotpost_data.sql
+++ Dcard_hotpost_data.sql
@@ -1,10 +1,10 @@
-SELECT P.id, P.title, P.content, P.forumAlias, P.forumName, P.rxnCount, 
+SELECT P.id, P.title, P.content, P.forum_alias, P.forum_name, P.rxn_count, 
 COUNT(IF(C.gender = 'M', 1, NULL)) AS Comment_M, 
 COUNT(IF(C.gender = 'F', 1, NULL)) AS Comment_F
-FROM media.DcardPost AS P
-LEFT JOIN media.DcardComment AS C
+FROM Dcard_Post AS P
+LEFT JOIN Dcard_Comment AS C
 ON P.id = C.postID
 WHERE P.createtime BETWEEN 1682870400 AND 1685548799
 AND (P.content LIKE '%電商%' OR P.content LIKE '%網購%' OR P.content LIKE '%團購%')
-GROUP BY P.id, P.title, P.content, P.forumAlias, P.forumName, P.rxnCount
-ORDER BY P.rxnCount DESC ;
+GROUP BY P.id, P.title, P.content, P.forum_alias, P.forum_name, P.rxn_count
+ORDER BY P.rxn_count DESC ;
